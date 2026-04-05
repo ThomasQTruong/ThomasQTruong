0cae2a731deac11dcc132e7f2e5bb5563ef5a7d2
Thomas Truong <TruongThomasQ@gmail.com>
2026-04-05 00:43:03 -0700

    feat: add ARIA accessibility and refactor nav logic

--- js/main.js
+++ js/main.js
@@ -1,37 +1,49 @@
-const navList = document.getElementById('nav-list');
-const projectsList = document.getElementById('projects-list');
+const navList = document.getElementById("nav-list");
+const projectsList = document.getElementById("projects-list");
+const projectsBtn = document.getElementById("projects-btn");
 
 
 function toggleSideBar(btn) {
-  const isOpen = navList.classList.toggle('show');
-  btn.classList.toggle('show');
+  const isOpen = navList.classList.toggle("show");
+  btn.classList.toggle("show");
 
-  // Nav bar was closed, make sure projects is closed too.
-  if (!isOpen) {
+  if (isOpen) {
+    // Update ARIA.
+    btn.setAttribute("aria-expanded", "true");
+  } else {
+    // Nav bar was closed, make sure projects is closed too.
     closeProjects();
+    // Update ARIA.
+    btn.setAttribute("aria-expanded", "false");
   }
 }
 
 function toggleProjects() {
-  const isOpen = projectsList.classList.toggle('show');
+  const isOpen = projectsList.classList.toggle("show");
 
-  // Projects is open, add listener for when user clicks away.
+  // Projects is open.
   if (isOpen) {
-    setTimeout(() => document.addEventListener('click', closeProjects), 0);
-  } else { 
-    // Projects is closed, remove listener.
-    document.removeEventListener('click', closeProjects);
+    // Add listener for when user clicks away.
+    setTimeout(() => document.addEventListener("click", closeProjects), 0);
+    // Update ARIA.
+    projectsBtn.setAttribute("aria-expanded", "true");
+  } else {
+    // Close the project since user toggled it off.
+    closeProjects()
   }
 }
 
 function closeProjects(event = null) {
   // Valid event AND clicked on one of the project related items.
-  if (event && projectsList.contains(event.target)) {
+  if (event && (projectsBtn.contains(event.target)
+                  || projectsList.contains(event.target))) {
     // Do nothing.
     return;
   }
 
   // User toggled or clicked somewhere else, close.
-  projectsList.classList.remove('show');
-  document.removeEventListener('click', closeProjects);
+  projectsList.classList.remove("show");
+  document.removeEventListener("click", closeProjects);
+  // Update ARIA.
+  projectsBtn.setAttribute("aria-expanded", "false");
 }
