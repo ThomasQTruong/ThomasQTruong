da74d14b15aef06ae00b44c5678fa04414c1dde0
Thomas Truong <TruongThomasQ@gmail.com>
2026-04-05 04:47:00 -0700

    feat: implement responsive state management and inert A11y

--- js/main.js
+++ js/main.js
@@ -1,20 +1,44 @@
 const navList = document.getElementById("nav-list");
+const navBtn = document.getElementById("sidebar-button");
 const projectsList = document.getElementById("projects-list");
 const projectsBtn = document.getElementById("projects-btn");
 
 
-function toggleSideBar(btn) {
+const media = window.matchMedia("(width < 480px)");
+let isMobile = media.matches;
+updateScreen();  // Initialize inert state.
+media.addEventListener("change", (e) => {
+  isMobile = e.matches;
+  updateScreen();
+});
+
+const navLinks = document.querySelectorAll("nav a");
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    if (isMobile && navList.classList.contains("show")) {
+      toggleSideBar();
+    }
+  });
+});
+
+
+function toggleSideBar() {
   const isOpen = navList.classList.toggle("show");
-  btn.classList.toggle("show");
+  navBtn.classList.toggle("show");
 
   if (isOpen) {
     // Update ARIA.
-    btn.setAttribute("aria-expanded", "true");
+    navBtn.setAttribute("aria-expanded", "true");
+    navList.removeAttribute("inert");
   } else {
     // Nav bar was closed, make sure projects is closed too.
     closeProjects();
     // Update ARIA.
-    btn.setAttribute("aria-expanded", "false");
+    navBtn.setAttribute("aria-expanded", "false");
+    if (isMobile) {
+      // User is on mobile, add inert.
+      navList.setAttribute("inert", "");
+    }
   }
 }
 
@@ -23,13 +47,16 @@ function toggleProjects() {
 
   // Projects is open.
   if (isOpen) {
+    // Remove listener first to prevent ghost listeners.
+    document.removeEventListener("click", closeProjects);
     // Add listener for when user clicks away.
     setTimeout(() => document.addEventListener("click", closeProjects), 0);
     // Update ARIA.
     projectsBtn.setAttribute("aria-expanded", "true");
+    projectsList.removeAttribute("inert");
   } else {
     // Close the project since user toggled it off.
-    closeProjects()
+    closeProjects();
   }
 }
 
@@ -46,4 +73,16 @@ function closeProjects(event = null) {
   document.removeEventListener("click", closeProjects);
   // Update ARIA.
   projectsBtn.setAttribute("aria-expanded", "false");
+  projectsList.setAttribute("inert", "");
+}
+
+function updateScreen() {
+  // User's screen size is mobile AND navList is hidden.
+  if (isMobile && !navList.classList.contains("show")) {
+    navList.setAttribute("inert", "");
+    closeProjects();
+  } else {
+    // User's screen size is not mobile, remove inert attribute.
+    navList.removeAttribute("inert");
+  }
 }
