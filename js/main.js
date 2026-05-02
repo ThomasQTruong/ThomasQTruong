8aa95b5b32b9fa75194a60076887fb690307c92b
Thomas Truong <TruongThomasQ@gmail.com>
2026-05-02 05:54:42 -0700

    feat(projects): add carousel images

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
-// DOM element selectors.
-const header = document.getElementById("header");
-const themeBtn = document.getElementById("theme-btn");
-const navList = document.getElementById("nav-list");
-const navBtn = document.getElementById("sidebar-btn");
-
-// Event listeners.
-navBtn.addEventListener("click", toggleSideBar);
-themeBtn.addEventListener("click", toggleTheme);
-
 // Variables.
 let scrollPosition = 0;
 let sidebarIsOpen = false;
 const media = window.matchMedia("(width >= 32em)");
 let isLargeScreen = media.matches;
 let resizeTimer;
+
+// DOM element selectors.
+const header = document.getElementById("header");
+const themeBtn = document.getElementById("theme-btn");
+const navList = document.getElementById("nav-list");
+const navBtn = document.getElementById("sidebar-btn");
+const navLinks = document.querySelectorAll("nav a");
+const sliders = document.querySelectorAll(".slider");
+const sliderImgs = document.querySelectorAll(".slider-img");
+const dots = document.querySelectorAll(".slider-dot");
+
+// Event listeners.
+navBtn.addEventListener("click", toggleSideBar);
+themeBtn.addEventListener("click", toggleTheme);
 
 // Listen to page size changes and update accordingly.
 updateScreen(); // Initialize inert state.
@@ -45,12 +49,93 @@ window.addEventListener("scroll", function () {
 });
 
 // Add listener to each nav link.
-const navLinks = document.querySelectorAll("nav a");
 navLinks.forEach((link) => {
   // User clicked link and sidebar is shown, hide sidebar.
   link.addEventListener("click", () => {
     if (navList.classList.contains("show")) {
       toggleSideBar();
+    }
+  });
+});
+
+// Add a listener for each slider.
+sliders.forEach((slider) => {
+  // When the user stops scrolling.
+  slider.addEventListener("scrollend", () => {
+    // Calculate current index.
+    const index = Math.round(slider.scrollLeft / slider.clientWidth);
+
+    // Set proper active state on dots.
+    const wrapper = slider.closest(".slider-wrapper");
+    const siblingDots = wrapper.querySelectorAll(".slider-dot");
+    if (siblingDots.length > 0) {
+      siblingDots.forEach((dot, i) => {
+        dot.classList.toggle("active", i === index);
+      });
+    }
+  });
+});
+
+// Add listener to each project slider's dot.
+dots.forEach((dot) => {
+  dot.addEventListener("click", (e) => {
+    const clickedDot = e.target;
+
+    // The image is already active.
+    if (clickedDot.classList.contains("active")) {
+      return;
+    }
+
+    const targetId = clickedDot.getAttribute("data-target");
+    const targetElement = document.getElementById(targetId);
+
+    // Scroll the image to the selected one if it exists.
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "nearest",
+        inline: "start",
+      });
+    }
+
+    // Remove the active class from the sibling dots.
+    const wrapper = clickedDot.closest(".slider-wrapper");
+    const siblingDots = wrapper.querySelectorAll(".slider-dot");
+    if (siblingDots.length > 0) {
+      siblingDots.forEach((dot) => dot.classList.remove("active"));
+
+      // Add active to the clicked dot.
+      clickedDot.classList.add("active");
+    }
+  });
+});
+
+// Add listener to each project image.
+sliderImgs.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    const clickedImg = e.currentTarget;
+    let nextImg = clickedImg.nextElementSibling;
+    // At the end or no other image to scroll through, cycle to the first.
+    if (!nextImg) {
+      nextImg = clickedImg.parentElement.firstElementChild;
+    }
+
+    // Scroll to the image.
+    const targetElement = nextImg.querySelector("img");
+    targetElement.scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+      inline: "start",
+    });
+
+    // Remove the active class from the sibling dots.
+    const wrapper = clickedImg.closest(".slider-wrapper");
+    const siblingDots = wrapper.querySelectorAll(".slider-dot");
+    if (siblingDots.length > 0) {
+      siblingDots.forEach((d) => d.classList.remove("active"));
+
+      // Add active to the related dot.
+      siblingDots[nextImg.getAttribute("index")].classList.add("active");
     }
   });
 });
